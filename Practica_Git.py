ebe931bd212dbc650bbc07f7be97f02cffad6396
Jea104 <telezleonardo@gmail.com>
2023-10-05 20:26:09 -0300

    Agrego Rama

--- Practica_Git.py
+++ Practica_Git.py
@@ -1,3 +1,5 @@
 print("hola este es la primera practica de git")
 print("este es el 2do cambio realizado")
 n = 5
+print("creacion de la primera rama")
+print("y ya pusheamos el repositorio a github")
