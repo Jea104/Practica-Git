e02bd468946755beb9e9aa3429ecd3fe96b301be
Jea104 <telezleonardo@gmail.com>
2023-10-05 19:08:51 -0300

    Cambios en comit

--- Practica_Git.py
+++ Practica_Git.py
@@ -1,1 +1,2 @@
 print("hola este es la primera practica de git")
+print("este es el segundo cambio realizado")
---
+++ segundo_archivo.py
@@ -0,0 +1,6 @@
+print("este es el segundo archivo")
+variable = 258
+numero = 11
+suma = variable + numero + 22
+print(suma)
+
